b9405fa83c79c10415fa24cdb2acbe94105ab946
ShibinBiju <shibinbijut101@gmail.com>
2026-02-26 19:45:05 +0530

    Initial commit

---
+++ app/Models/Vote.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vote extends Model
+{
+
+public $fillable = [
+    'user_id',
+    'candidate_id',
+];
+    
+
+public function user()
+{
+    return $this->belongsTo(User::class);
+}
+
+public function candidate()
+{
+    return $this->belongsTo(Candidate::class);
+}
+}
